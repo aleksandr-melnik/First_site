ebcc25f3bb9ceffb7546182e6f0292665b1ad3ec
Александр Мельник <melnik.aleksandr.02@gmail.com>
2020-04-30 17:34:22 +0300

    Fitst_site

--- my_first_site/pages/views.py
+++ my_first_site/pages/views.py
@@ -4,11 +4,18 @@ from django.http import HttpResponse
 def index(request):
     name = request.GET.get('name')
     type = request.GET.get('type')
-    humanoid = True
+    humanoid = request.GET.get('humanoid')
     name_length = len(name)
+    # humanoid = True
+    if humanoid == '1':
+        humanoid = "humanoid Да"
+    elif humanoid == '0':
+        humanoid = 'humanoid Нет'
+
     context = {'name': name,
                'type': type,
-               'name_length': name_length
+               'name_length': name_length,
+               'humanoid': humanoid,
                               }
     return render(request, 'index.html', context=context)
 
